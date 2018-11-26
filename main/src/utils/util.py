8124bca01dfdf59eb5755fcadb43d8c07bae189b
AntonPon <ponomarchuk.anton@gmail.com>
2018-11-26 15:31:57 +0200

    added image rendering in tensorbard

--- main/src/utils/util.py
+++ main/src/utils/util.py
@@ -2,6 +2,8 @@
 Misc Utility functions
 '''
 import os
+import torch
+
 
 def recursive_glob(rootdir='.', suffix=''):
     """Performs recursive glob with given suffix and rootdir
@@ -17,5 +19,16 @@ def recursive_glob(rootdir='.', suffix=''):
 
 
 def add_info(writer, epoch, loss=0, miou=0, mode='train'):
-    writer.add_scalar('{}_loss'.format(mode), loss, epoch)
-    writer.add_scalar('{}_miou'.format(mode), miou, epoch)
+    writer.add_scalar('loss/{}'.format(mode), loss, epoch)
+    writer.add_scalar('miou/{}'.format(mode), miou, epoch)
+
+
+def add_image(writer, epoch, images):
+    writer.add_image('test images', images, epoch)
+
+
+def save_model(epoch, model_state, optimizer_state, model='fpn', dataset='cityscapes'):
+    state = {'epoch': epoch + 1,
+             'model_state': model_state,
+             'optimizer_state': optimizer_state, }
+    torch.save(state, "{}_{}_best_model_iou.pkl".format(model, dataset))
